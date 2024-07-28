4cd8b96c3fa39fabdbd41a44cd42439472521b3c
bunsal.infinity <bunsal141.infinity@gmail.com>
2024-07-28 15:42:50 +0700

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,24 @@
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 12,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
   rules: {
-    indent: ["error", 2],
+    indent: ['error', 2],
+    'no-unused-vars': ['warn'],
+    'no-console': 'off',
+    'no-debugger': 'off',
   },
 };
